bfb59685a189e595d84a656ef54dc48f757dd96e
fbcsec <fbcsec@github.com>
2019-12-07 12:44:02 -0500

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+
+sudo apt-get install git build-essential zsh zsh-autosuggestions zsh-syntax-highlighting neovim gdb fonts-go -y
+
+git clone https://github.com/zsh-users/zsh-history-substring-search.git /usr/share/zsh/plugins/zsh-history-substring-search
+cp ./rcfiles/.zshrc ~/.zshrc
+
+update-alternatives --set vim $(which nvim)
+mkdir -p ~/.config/nvim
+cp ./rcfiles/init.vim ~/.config/nvim/init.vim
+
+git clone https://github.com/cyrus-and/gdb-dashboard.git /tmp/gdb-dashboard
+cp /tmp/gdb-dashboard/.gdbinit ~/.gdbinit
+rm -r /tmp/gdb-dashboard
---
+++ rcfiles/init.vim
@@ -0,0 +1,14 @@
+set mouse=""
+syntax enable
+set tabstop=4
+set softtabstop=4
+set expandtab
+set wildmenu
+set showmatch
+filetype indent on
+set number
+set lazyredraw
+set hlsearch
+set backspace=indent,eol,start
+set ruler
+set undofile
