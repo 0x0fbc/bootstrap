d38bbc53d60aa8fd7413f9606ff1faadf8d5040f
0x0fbc <0x0fbc@github.com>
2020-05-16 03:46:34 -0400

    add bspwm

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,18 +2,23 @@ sudo apt-get update
 sudo apt-get dist-upgrade -y
 
 sudo apt-get install git build-essential zsh zsh-autosuggestions zsh-syntax-highlighting tmux neovim gdb fonts-ibm-plex -y
-
+sudo apt-get install bspwm sxhkd dunst polybar qterminal
 
 sudo mkdir -p /usr/share/zsh/plugins
 sudo git clone https://github.com/zsh-users/zsh-history-substring-search.git /usr/share/zsh/plugins/zsh-history-substring-search
 cp ./rcfiles/.zshrc ~/.zshrc
 
-update-alternatives --set vim $(which nvim)
+sudo update-alternatives --set vim $(which nvim)
 mkdir -p ~/.config/nvim
 cp ./rcfiles/init.vim ~/.config/nvim/init.vim
 
-git clone https://github.com/cyrus-and/gdb-dashboard.git /tmp/gdb-dashboard
-cp /tmp/gdb-dashboard/.gdbinit ~/.gdbinit
-rm -rf /tmp/gdb-dashboard
+sudo apt-get install python3 python3-pip
+pip3 install gdbgui
 
-cp ./rcfiles/.tmux.conf ~/.tmux.conf
+mkdir -p ~/.config/qterminal.org
+rm -f ~/.config/qterminal.org/qterminal.ini
+cp ./rcfiles/qterminal.ini ~/.config/qterminal.org/qterminal.ini
+
+cp -r ./rcfiles/bspwm ~/.config/bspwm
+cp -r ./rcfiles/sxhkd ~/.config/sxhkd
+cp -r ./rcfiles/polybar ~/.config/polybar
